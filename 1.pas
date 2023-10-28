9581d48f3ade4b8045a619dc5cf5742c5060065a
Vicketori <144226383+Vicketori@users.noreply.github.com>
2023-10-28 18:32:34 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,21 @@
+﻿var
+  x, y: real;
+
+begin
+  write('x = ');
+  readln(x);
+  if x < -6
+    then y := exp(x * ln(5)) + exp(x * ln(x / 10)) / 23
+  else 
+  if x < -4 
+    then y := sin(x) * ln(x) / ln(10)
+    else 
+  if x < 2
+    then y := x * x / cos(x) - exp(x * ln(0.1 * x)) / (ln(x) / ln(10))
+      else 
+  if x >= 2
+    then y := ln(x) / ln(10) * cos(2 * x) + exp(x * ln(1 / 3)) / x / x;
+  if y = y
+    then writeln('При x = ', x, ', y = ', y)
+  else writeln('При x = ', x, ', нет решений.');
+end.
---
+++ 2.pas
@@ -0,0 +1,25 @@
+﻿var
+  x1, x2, y: real;
+
+begin
+  x1 := -8 - 0.3;
+  x2 := 4 - 0.3;
+  while x1 < x2 do
+  begin
+    if x1 < -6
+      then y := exp(x1 * ln(5)) + exp(x1 * ln(x1 / 10)) / 23
+    else 
+    if x1 < -4 
+      then y := sin(x1) * ln(x1) / ln(10)
+    else 
+    if x1 < 2
+      then y := x1 * x1 / cos(x1) - exp(x1 * ln(0.1 * x1)) / (ln(x1) / ln(10))
+      else 
+    if x1 >= 2
+      then y := ln(x1) / ln(10) * cos(2 * x1) + exp(x1 * ln(1 / 3)) / x1 / x1;
+    x1 := x1 + 0.3;
+    if y = y
+      then writeln(x1:4:1, y:20:3)
+    else writeln(x1:4:1, '               нет решений')
+  end;
+end.
